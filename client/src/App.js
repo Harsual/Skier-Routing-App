e68ea7af3a0b8219bfba365ed8809791de1e7a16
Osn132 <40571631+Harsual@users.noreply.github.com>
2024-03-03 16:02:53 -0330

    static front page

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import React, {useEffect, useState} from 'react'
+import './App.css'
+
 
 function App() {
   
@@ -15,8 +17,22 @@ function App() {
   },[])
   
   return (
-    <div >
-      
+    <div>
+      <div className="container">
+        
+        <header>Weclome to the Ski-resort App</header>
+        
+        <div className='image-container'>
+          <img src="Map.jpeg" className= "image" alt="Example" />
+        </div>
+  
+        
+        <div>
+        <button className='large-button'>Find Path</button>
+        <button className='large-button'>Find Facility </button>
+        <button className='large-button'>Use lift to move up</button>
+        </div> 
+        </div>
     </div>
   )
 }
